464624cb7d15ed7c23130b4ced73ced3385792e9
Eugenio Regis <kuque84@hotmail.com>
2024-11-08 12:10:45 -0300

    Impresión en PDF de Acta de Examen completa

--- src/components/Examenes/ActaExamen.jsx
+++ src/components/Examenes/ActaExamen.jsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { fetchCalificacion } from "../../functions/previa.function";
-import axios from "axios";
-import settings from "../../Config/index";
-import Swal from "sweetalert2";
+import React, { useEffect, useState } from 'react';
+import { fetchCalificacion } from '../../functions/previa.function';
+import axios from 'axios';
+import settings from '../../Config/index';
+import Swal from 'sweetalert2';
 import { downloadPDF } from '../../functions/downloadPDF';
 
 const API_URL = settings.API_URL;
 
-const ActaExamen = ({ examen }) => {
+const ActaExamen = ({ examen, actadeexamen }) => {
   const [calificacion, setCalificacion] = useState([]);
   const [id_calificacion, setid_calificacion] = useState(
-    examen.map((e) => e.id_calificacion || "")
+    examen.map((e) => e.id_calificacion || '')
   );
-  const [libro, setLibro] = useState(examen[0].libro || "");
-  const [folio, setFolio] = useState(examen[0].folio || "");
+  const [libro, setLibro] = useState(examen[0].libro || '');
+  const [folio, setFolio] = useState(examen[0].folio || '');
   const [fechaExamen, setFechaExamen] = useState(
     examen[0].FechaExamen.fechaExamen
       ? new Date(examen[0].FechaExamen.fechaExamen).toISOString().slice(0, 16)
-      : ""
+      : ''
   );
   useEffect(() => {
     const loadCalificacion = async () => {
@@ -28,84 +28,8 @@ const ActaExamen = ({ examen }) => {
     // console.log("Examen: ");
     // console.table(examen);
     // console.log("Calificacion: ");
-    // console.table(calificacion); 
+    // console.table(calificacion);
   }, []);
-
-  const handleGuardar1 = () => {
-    const acta = examen.map((e, index) => ({
-      id_inscripcion: e.id_inscripcion,
-      id_calificacion: id_calificacion[index],
-      id_fechaExamen: examen[index].id_fechaExamen,
-      libro: libro,
-      folio: folio,
-    }));
-
-    const previa = examen.map((e, index) => ({
-      id_previa: e.id_previa,
-      id_calificacion: id_calificacion[index],
-
-    }));
-
-
-    let fechaExamenLocal = new Date(fechaExamen);
-    fechaExamenLocal.setMinutes(
-      fechaExamenLocal.getMinutes() - fechaExamenLocal.getTimezoneOffset()
-    );
-
-    const fecha = {
-      id_fechaExamen: examen[0].id_fechaExamen,
-      fechaExamen: fechaExamenLocal.toISOString(),
-    };
-
-    //console.log("fecha: ", fecha);
-
-    acta.forEach(async (item) => {
-
-      await axios
-        .put(`${API_URL}/fechaexamen/actualizar/${item.id_fechaExamen}`, fecha, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        })
-        .then((response) => {
-          Swal.fire("Datos actualizados", "Datos almacenados con éxito", "success");
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-        console.log("Calificacion: ");
-        console.table(calificacion);
-        console.log("item.id_calificacion: ", item.id_calificacion);
-
-        let calificacionEncontrada = calificacion.find((calificacionItem) => calificacionItem.id_calificacion === item.id_calificacion);
-        if(calificacionEncontrada) {
-            console.log("calificacion.aprobado: ", calificacionEncontrada.aprobado);
-        } else {
-            console.log("No se encontró la calificación correspondiente");
-        }
-
-        
-      await axios
-        .put(`${API_URL}/inscripcion/actualizar/${item.id_inscripcion}`, item, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        })
-        .then((response) => {
-          Swal.fire("Datos actualizados", "Datos almacenados con éxito", "success");
-        })
-        .catch((error) => {
-          console.log(error);
-
-          if (error.response && error.response.status === 409) {
-            Swal.fire("No se pudo editar el acta", `${error.response.data.message}`, "warning");
-          } else {
-            Swal.fire("Error", "Hubo un error al editar el acta", "error");
-          }
-        });
-    });
-  };
 
   const handleGuardar = () => {
     let fechaExamenLocal = new Date(fechaExamen);
@@ -136,17 +60,17 @@ const ActaExamen = ({ examen }) => {
       previa: previa,
     };
 
-    console.log("Acta: ");
+    console.log('Acta: ');
     console.table(acta);
 
     axios
       .put(`${API_URL}/inscripcion/acta/`, acta, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
       })
       .then((response) => {
-        Swal.fire("Datos actualizados", "Datos almacenados con éxito", "success");
+        Swal.fire('Datos actualizados', 'Datos almacenados con éxito', 'success');
       })
       .catch((error) => {
         console.error('Error al actualizar el acta:', error.message);
@@ -162,54 +86,94 @@ const ActaExamen = ({ examen }) => {
           // Algo ocurrió al configurar la solicitud que lanzó un error
           console.error('Error en la solicitud:', error.message);
         }
-        Swal.fire("Error", "No se pudo actualizar los datos. Por favor, inténtelo nuevamente.", "error");
+        Swal.fire(
+          'Error',
+          'No se pudo actualizar los datos. Por favor, inténtelo nuevamente.',
+          'error'
+        );
       });
+  };
 
-};
+  const handleDownload = () => {
+    const id_turno = actadeexamen[0];
+    const id_condicion = actadeexamen[1];
+    const id_materia = actadeexamen[2];
+    const anio = actadeexamen[3];
+    /*
+    console.log('id_turno:', id_turno);
+    console.log('id_condicion:', id_condicion);
+    console.log('id_materia:', id_materia);
+    */
+    console.log(
+      'Acta:',
+      ' - ',
+      examen[0].Previa.Condicion.nombre,
+      ' - ',
+      examen[0].Previa.Curso.nombre,
+      ' - ',
+      examen[0].Previa.Materia.nombre,
+      ' - ',
+      examen[0].Previa.Plan.codigo,
+      ' - ',
+      anio
+    );
+    console.log(examen);
+    //consultar en backend el nombre del id_turno, id_condicion, id_materia
 
-const handleDownload = () => {
-  const endpoint = `/acta/examen/pdf`; // Ajusta el endpoint según tu configuración backend
-  const filename = 'acta_examen.pdf';
-  downloadPDF(endpoint, filename);
-};
+    const endpoint = `/acta/examen/pdf/${id_turno}/${id_condicion}/${id_materia}`; // Ajusta el endpoint según tu configuración backend]);
+    //const endpoint = `/acta/examen/pdf`; // Ajusta el endpoint según tu configuración backend
+    const filename =
+      examen[0].Previa.Condicion.nombre +
+      '-' +
+      examen[0].Previa.Curso.nombre +
+      '-' +
+      examen[0].Previa.Materia.nombre +
+      '-' +
+      examen[0].Previa.Plan.codigo +
+      '-' +
+      anio +
+      '.pdf';
+
+    downloadPDF(endpoint, filename);
+  };
 
   return (
-    <div className=" bg-sky-100 border border-secondary rounded-md p-8 shadow-lg backdrop:filter backdrop-blur-sm bg-opacity-60 relative font-semibold mt-4 mb-6">
-      <h1 className="bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text text-xl sm:text-3xl lg:text-4xl text-center tracking-wide py-2">
+    <div className=' bg-sky-100 border border-secondary rounded-md p-8 shadow-lg backdrop:filter backdrop-blur-sm bg-opacity-60 relative font-semibold mt-4 mb-6'>
+      <h1 className='bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text text-xl sm:text-3xl lg:text-4xl text-center tracking-wide py-2'>
         Acta de Examen
       </h1>
-      <div className="relative mt-4 mb-6">
+      <div className='relative mt-4 mb-6'>
         {/* Lista de previas */}
-        <table className="min-w-full py-1 px-0 text-sm text-secondary bg-transparent border-2 border-primary appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer">
+        <table className='min-w-full py-1 px-0 text-sm text-secondary bg-transparent border-2 border-primary appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer'>
           <thead>
             <tr>
-              <th className="text-center border-2 border-primary">N°</th>
-              <th className="text-center border-2 border-primary">DNI</th>
-              <th className="text-center border-2 border-primary">Apellido y Nombre</th>
-              <th className="text-center border-2 border-primary">Calificación</th>
+              <th className='text-center border-2 border-primary'>N°</th>
+              <th className='text-center border-2 border-primary'>DNI</th>
+              <th className='text-center border-2 border-primary'>Apellido y Nombre</th>
+              <th className='text-center border-2 border-primary'>Calificación</th>
             </tr>
           </thead>
           <tbody>
             {examen.map((examen, index) => (
               <tr key={examen.id_inscripcion}>
-                <td className="text-center border-dotted border-2 border-primary">
-                  {String(index + 1).padStart(2, "0")}
+                <td className='text-center border-dotted border-2 border-primary'>
+                  {String(index + 1).padStart(2, '0')}
                 </td>
-                <td className="text-center border-dotted border-2 border-primary">
+                <td className='text-center border-dotted border-2 border-primary'>
                   {examen.Previa.Alumno.dni}
                 </td>
-                <td className="text-center border-dotted border-2 border-primary">
+                <td className='text-center border-dotted border-2 border-primary'>
                   {examen.Previa.Alumno.apellidos}, {examen.Previa.Alumno.nombres}
                 </td>
-                <td className="text-center border-dotted border-2 border-primary">
+                <td className='text-center border-dotted border-2 border-primary'>
                   <select
                     onChange={(e) => {
                       const newId_calificacion = [...id_calificacion];
                       newId_calificacion[index] = e.target.value;
                       setid_calificacion(newId_calificacion);
                     }}
-                    value={id_calificacion[index] || ""}
-                    className="block py-1 px-0 w-full text-center text-base text-secondary bg-transparent border-0 appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer"
+                    value={id_calificacion[index] || ''}
+                    className='block py-1 px-0 w-full text-center text-base text-secondary bg-transparent border-0 appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer'
                   >
                     {calificacion.map((calificacion) => (
                       <option
@@ -225,93 +189,88 @@ const handleDownload = () => {
             ))}
           </tbody>
         </table>
-        <div className="pt-3">
-          <div className="relative mt-4 mb-6">
+        <div className='pt-3'>
+          <div className='relative mt-4 mb-6'>
             <input
-              id="libro"
-              className="block py-0 px-0 w-full text-base text-secondary bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer"
+              id='libro'
+              className='block py-0 px-0 w-full text-base text-secondary bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer'
               value={libro}
-              type="text"
-              autoComplete="off"
-              name="libro"
-              placeholder=""
+              type='text'
+              autoComplete='off'
+              name='libro'
+              placeholder=''
               onChange={(e) => setLibro(e.target.value.toUpperCase())}
               //se debe completar el campo dni
               required
             />
             <label
-              htmlFor="libro"
+              htmlFor='libro'
               className={`peer-focus:font-medium absolute text-sm text-primary duration-300 transform ${
-                libro ? "-translate-y-6 scale-75" : "-translate-y-1 scale-100"
+                libro ? '-translate-y-6 scale-75' : '-translate-y-1 scale-100'
               } top-1 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-secondary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0`}
             >
               Libro:
             </label>
           </div>
-          <div className="relative mt-4 mb-6">
+          <div className='relative mt-4 mb-6'>
             <input
-              id="folio"
-              className="block py-0 px-0 w-full text-base text-secondary bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer"
+              id='folio'
+              className='block py-0 px-0 w-full text-base text-secondary bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer'
               value={folio}
-              type="text"
-              autoComplete="off"
-              name="folio"
-              placeholder=""
+              type='text'
+              autoComplete='off'
+              name='folio'
+              placeholder=''
               onChange={(e) => setFolio(e.target.value.toUpperCase())}
               //se debe completar el campo dni
               required
             />
             <label
-              htmlFor="folio"
+              htmlFor='folio'
               className={`peer-focus:font-medium absolute text-sm text-primary duration-300 transform ${
-                folio ? "-translate-y-6 scale-75" : "-translate-y-1 scale-100"
+                folio ? '-translate-y-6 scale-75' : '-translate-y-1 scale-100'
               } top-1 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-secondary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0`}
             >
               Folio:
             </label>
           </div>
-          <div className="relative mt-4 mb-6">
+          <div className='relative mt-4 mb-6'>
             <input
-              id="fechaExamen"
-              className="block py-0 px-0 w-full text-base text-secondary bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer"
+              id='fechaExamen'
+              className='block py-0 px-0 w-full text-base text-secondary bg-transparent border-0 border-b-2 border-primary appearance-none focus:outline-none focus:ring-0 focus:border-secondary peer'
               value={fechaExamen}
-              type="datetime-local"
-              autoComplete="off"
-              name="fechaExamen"
-              placeholder=""
+              type='datetime-local'
+              autoComplete='off'
+              name='fechaExamen'
+              placeholder=''
               onChange={(e) => setFechaExamen(e.target.value)}
               required
             />
             <label
-              htmlFor="fechaExamen"
+              htmlFor='fechaExamen'
               className={`peer-focus:font-medium absolute text-sm text-primary duration-300 transform ${
-                fechaExamen ? "-translate-y-6 scale-75" : "-translate-y-1 scale-100"
+                fechaExamen ? '-translate-y-6 scale-75' : '-translate-y-1 scale-100'
               } top-1 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-secondary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0`}
             >
               Fecha y Hora:
             </label>
           </div>
           <button
-            type="button"
+            type='button'
             onClick={() => {
               handleGuardar();
             }}
-            className="print:hidden ml-5 text-xs sm:text-sm lg:text-base z-10 border border-success p-3 my-4 text-black dark:text-white hover:text-white dark:hover:text-black rounded-md hover:bg-gradient-to-r from-success to-green-500 ease-in duration-300"
+            className='print:hidden ml-5 text-xs sm:text-sm lg:text-base z-10 border border-success p-3 my-4 text-black dark:text-white hover:text-white dark:hover:text-black rounded-md hover:bg-gradient-to-r from-success to-green-500 ease-in duration-300'
           >
             Guardar
           </button>
+
           <button
-            type="button"
-            onClick={() => window.print()}
-            className="print:hidden ml-5 text-xs sm:text-sm lg:text-base z-10 border border-info p-3 my-4 text-black dark:text-white hover:text-white dark:hover:text-black rounded-md hover:bg-gradient-to-r from-info to-blue-800 ease-in duration-300"
+            type='button'
+            onClick={() => handleDownload()}
+            className='print:hidden ml-5 text-xs sm:text-sm lg:text-base z-10 border border-info p-3 my-4 text-black dark:text-white hover:text-white dark:hover:text-black rounded-md hover:bg-gradient-to-r from-info to-blue-800 ease-in duration-300'
           >
             Imprimir
-          </button>
-
-          <button 
-            onClick={handleDownload}
-          >
-            Descargar PDF
           </button>
         </div>
       </div>
--- src/functions/previa.function.jsx
+++ src/functions/previa.function.jsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import settings from "../Config/index";
+import axios from 'axios';
+import settings from '../Config/index';
 
 const API_URL = settings.API_URL;
 
@@ -7,7 +7,7 @@ export const fetchData = async (endpoint) => {
   try {
     const response = await axios.get(`${API_URL}${endpoint}`, {
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
     });
     //console.log(`Datos de ${endpoint}`, response.data);
@@ -19,45 +19,67 @@ export const fetchData = async (endpoint) => {
 };
 
 export const fetchCurso = (id_plan) => fetchData(`/curso/filtrar/id_plan/${id_plan}`);
+export const fetchCursoByCicloLectivo = (id_ciclo) =>
+  fetchData(`/inscripcion/filtrar/id_ciclo/${id_ciclo}`);
 export const fetchCicloLectivos = () => fetchData('/ciclolectivo/lista');
 export const fetchPlan = () => fetchData('/plan/lista');
 export const fetchMateria = (id_curso) => fetchData(`/materia/filtrar/id_curso/${id_curso}`);
 export const fetchCondicion = () => fetchData('/condicion/lista');
 export const fetchPrevias = (id_alumno) => fetchData(`/previa/filtrar/id_alumno/${id_alumno}`);
 export const fetchTurno = (id_ciclo) => fetchData(`/turnoexamen/filtrar/id_ciclo/${id_ciclo}`);
-export const fetchInscripcion = (id_turno) => fetchData('/inscripcion/filtrar/id_turno/' + id_turno);
+export const fetchInscripcion = (id_turno) =>
+  fetchData('/inscripcion/filtrar/id_turno/' + id_turno);
 export const fetchCalificacion = () => fetchData('/calificacion/lista');
 export const fetchRac = (id_alumno) => fetchData(`/inscripcion/rac/${id_alumno}`);
 
+export const fetchCicloByTurno = () => fetchData('/actaexamen/ciclo');
+export const fetchTurnoByCiclo = (id_ciclo) => fetchData(`/actaexamen/turno/id_ciclo/${id_ciclo}`);
+export const fetchCondicionByTurnoAndCiclo = (id_turno, id_ciclo) =>
+  fetchData(`/actaexamen/condicion/${id_turno}/${id_ciclo}`);
+export const fetchCursoByCicloTurnoAndCondicion = (id_ciclo, id_turno, id_condicion) =>
+  fetchData(`/actaexamen/curso/${id_ciclo}/${id_turno}/${id_condicion}`);
+export const fetchMateriaByCicloTurnoCondicionAndCurso = (
+  id_ciclo,
+  id_turno,
+  id_condicion,
+  id_curso
+) => fetchData(`/actaexamen/materia/${id_ciclo}/${id_turno}/${id_condicion}/${id_curso}`);
+
+export const fetchTurnoByInscripcion = (id_ciclo) =>
+  fetchData(`/turnoexamen/filtrar/id_ciclo/${id_ciclo}`);
+
 export const fetchFechaExamenInscripcion = async (id_materia, id_turno) => {
   try {
-    const response = await axios.get(`${API_URL}/fechaexamen/filtrar/inscripcion/${id_turno}/${id_materia}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
-    console.log("Datos de fecha de examen:", response
+    const response = await axios.get(
+      `${API_URL}/fechaexamen/filtrar/inscripcion/${id_turno}/${id_materia}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      }
     );
+    console.log('Datos de fecha de examen:', response);
     return response.data;
-  }
-  catch (err) {
-    console.error("Error al obtener la fecha de examen:", err);
+  } catch (err) {
+    console.error('Error al obtener la fecha de examen:', err);
     throw err;
   }
-}
+};
 
 export const fetchActaExamen = async (id_turno, id_condicion, id_materia) => {
   try {
-    const response = await axios.get(`${API_URL}/inscripcion/filtrar/acta/${id_turno}/${id_condicion}/${id_materia}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
-    console.log("Datos de acta de examen:", response);
+    const response = await axios.get(
+      `${API_URL}/inscripcion/filtrar/acta/${id_turno}/${id_condicion}/${id_materia}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      }
+    );
+    console.log('Datos de acta de examen:', response);
     return response.data;
-  }
-  catch (err) {
-    console.error("Error al obtener la acta de examen:", err);
+  } catch (err) {
+    console.error('Error al obtener la acta de examen:', err);
     throw err;
   }
-}
+};
